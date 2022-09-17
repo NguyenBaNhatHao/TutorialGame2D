539382a06d38a67eb913e8d025b67e7e5498a463
NguyenBaNhatHao <jdjjdjjdjdjdjddd@gmail.com>
2022-09-17 09:55:29 +0700

    backup

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,7 +9,9 @@ public class PlayerMovement : MonoBehaviour
     private Animator anim;
     private float dirX = 0f;
     [SerializeField] private float MoveSpeed = 14f;
-    [SerializeField] private float JumpForce = 7f;
+    [SerializeField] private float JumpForce = 15f;
+    
+    private enum MovementState { idle, running, jumping, falling }
     // Start is called before the first frame update
     void Start()
     {
@@ -32,19 +34,28 @@ public class PlayerMovement : MonoBehaviour
     }
     private void UpdateAnimation()
     {
+        MovementState state;
         if (dirX > 0f)
         {
-            anim.SetBool("Running", true);
+            state = MovementState.running;
             sprite.flipX = false;
         }
         else if (dirX < 0f)
         {
-            anim.SetBool("Running", true);
+            state = MovementState.running;
             sprite.flipX = true;
         }
         else
         {
-            anim.SetBool("Running", false);
+            state = MovementState.idle;
         }
+        if(rb.velocity.y > .1f)
+        {
+            state = MovementState.jumping;
+        }else if(rb.velocity.y < -.1f)
+        {
+            state = MovementState.falling;
+        }
+        anim.SetInteger("state", (int)state);
     }
 }
